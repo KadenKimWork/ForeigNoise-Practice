c734d4464f9130a9a62133d7388eb9b7c573007c
KadenKimWork <KadenKimWork@gmail.com>
2025-06-07 21:18:52 +0900

    add members_button click
    Added functionality for members_list.textContent to = the names of members.
    Added functionality for the done_button to make members_list.textContent an empty string.

---
+++ script.js
@@ -0,0 +1,14 @@
+let members_button = document.querySelector("#members_button");
+let members_list = document.querySelector("#members_list");
+
+members_button.addEventListener("click", () => {
+
+    members_list.textContent = "CozyFoundViper";
+
+});
+
+document.querySelector("#done_button").addEventListener("click", () => {
+
+    members_list.textContent = "";
+
+});
