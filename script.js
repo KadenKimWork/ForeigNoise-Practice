4fb41d07d773385edc97dd2c900f63d700934813
KadenKimWork <KadenKimWork@gmail.com>
2025-06-07 21:45:00 +0900

    Add .textContent for buttons
    Made .textContent of desired <div> = to some nice example text.
    Made all other .textContent of undesired <div> = to an empty string, thus showing only the contents of the information the user would like to see.

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
-/*
+
+let crew_bio_button = document.querySelector("#crew_bio_button");
 let members_button = document.querySelector("#members_button");
 let achievements_button = document.querySelector("#achievements_button");
 let media_button = document.querySelector("#media_button");
-*/
 
+
+let crew_bio = document.querySelector("#crew_bio");
 let members_list = document.querySelector("#members_list");
+let achievements_list = document.querySelector("#achievements_list");
+let media_list = document.querySelector("#media_list");
+
+let found_bio = document.querySelector("#found");
+
+
+crew_bio_button.addEventListener("click", () => {
+    crew_bio.textContent = "ForeigNoise is a worldwide breaking team. Founded in 2019, it served as a community of both foreign and Korean breakers to bridge cultural differences through Hip Hop culture and dance. While it started off as a forum for the foreign-breaking community to come together to learn from one another, battle, and prepare for competitions, ForeigNoise has evolved into its current form to educate and entertain public and private audiences about breaking and Hip Hop culture. Its current membership includes members of the Korean diaspora from the Philippines and the United States.";
+    members_list.textContent = "";
+    achievements_list.textContent = "";
+    media_list.textContent = "";
+});
 
 members_button.addEventListener("click", () => {
+    crew_bio.textContent = "";
+    members_list.textContent = found_bio.textContent;
+    achievements_list.textContent = "";
+    media_list.textContent = "";
 
-    members_list.textContent = "CozyFoundViper";
 
 });
 
+achievements_button.addEventListener("click", () => {
+    crew_bio.textContent = "";
+    members_list.textContent = "";
+    achievements_list.textContent = "Taco Amigo, China UN, Holi Hai";
+    media_list.textContent = "";
+});
+
+media_button.addEventListener("click", () => {
+    crew_bio.textContent = "";
+    members_list.textContent = "";
+    achievements_list.textContent = "";
+    media_list.textContent = "PHOTO AND VIDEO PLACEHOLDER";
+})
+
+
+
+
 document.querySelector("#done_button").addEventListener("click", () => {
 
+    crew_bio.textContent = "";
     members_list.textContent = "";
+    achievements_list.textContent = "";
+    media_list.textContent = "";
 
 
 });
