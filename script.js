b58262e49b112736121910cea9b883e00b266941
KadenKimWork <KadenKimWork@gmail.com>
2025-06-13 23:02:41 +0900

    Add event descriptions and divs

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ document.querySelector("#buttons").addEventListener("click", e => {
     } else if (e.target == members_button) {
         //main_space.textContent = document.querySelector("#cozy_name").innerHTML + document.querySelector("#cozy_name").textContent;
         
-        main_space_div.innerHTML = '<div id="members_list"><p id="cozy_name"><img src="Cozy_Photo.png" style="width: 70px; height: auto;">COZY</p><p id="found_name">FOUND</p><p id="viper_name">VIPER</p></div>'
+        main_space_div.innerHTML = '<div id="members_list"><p id="cozy_name"><img src="Cozy_Photo.png" style="width: 70px; height: auto;">COZY</p><p id="found_name"><img src="Found_Photo.png" style="width: 70px; height: auto">FOUND</p><p id="viper_name"><img src="Viper_Photo.png" style="width: 70px; height: auto;">VIPER</p></div>'
 
             /*
             document.querySelector("#members_list").addEventListener("click", e => {
@@ -106,19 +106,19 @@ document.querySelector("#buttons").addEventListener("click", e => {
             */
 
         document.querySelector("#cozy_name").addEventListener("click", () => {
-            main_space_div.innerHTML = '<p id="cozy_bio"><img src="Cozy_Photo.png">Cozy age 36</p>'
+            main_space_div.innerHTML = '<p id="cozy_bio"><img src="Cozy_Photo.png" style="width: 100px; height: auto;">Cozy age 36</p>'
         });
         document.querySelector("#found_name").addEventListener("click", () => {
-            main_space_div.innerHTML = '<p id="found_bio">Found age 31</p>'
+            main_space_div.innerHTML = '<p id="found_bio"><img src="Found_Photo.png" style="width: 100px; height: auto;">Found age 31</p>'
         });
         document.querySelector("#viper_name").addEventListener("click", () => {
-            main_space_div.innerHTML = '<p id="viper_name">Viper age 28</p>'            
+            main_space_div.innerHTML = '<p id="viper_name"><img src="Viper_Photo.png" style="width: 100px; height: auto;">Viper age 28</p>'            
         })
 
 
     } else if (e.target == achievements_button) {
         //main_space.textContent = achievements_list.textContent;
-        main_space_div.innerHTML = '<p id="achievements list">TACO AMIGO, CHINA UN, HOLI HAI</p>'
+        main_space_div.innerHTML = '<div id="achievements"><p id="achievements list"><div id="taco_amigo"><p>TACO AMIGO</p><p>A charming hour long show for the excited guests of "Taco Amigo" in Itaewon, South Korea! Guests were blown away by the members of ForeigNoise&apos;s performances and were even invited to learn some of their own Breaking moves where they would battle it out for a special prize!</p></div><div id="china_un"><p>CHINA UN</p><p>An uplifting performance celebrating the contestants who worked hard to compete at the China UN&apos;s Korean language competition. Being no strange to professionalism, ForeigNoise brought the nervous crowd to a stand with their energetic moves and routines!</p></div><div id="holi_hai"><p>HOLI HAI</p><p>A captivating and highly dynamic performance that captured the spirit of "Holi"! ForeigNoise was invited to Chuncheon, South Korea where they would celebrate the Hindu festival of spring, or "festival of colors", and light up the crowd!</p></div></div>'
 
     } else if (e.target == media_button) {
         //main_space.textContent = "VIDEO";
