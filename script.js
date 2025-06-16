ac7ef6147adc5073122795b44d5ca4e5ea45073d
KadenKimWork <KadenKimWork@gmail.com>
2025-06-17 02:14:03 +0900

    Formatting and positioning

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let crew_head = document.querySelector("#crew_head");
+
 let menu_button = document.querySelector("#menu_button");
 let crew_bio_button = document.querySelector("#crew_bio_button");
 let members_button = document.querySelector("#members_button");
